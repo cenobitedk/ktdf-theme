88a82eb94f0a849e0bf8940feb1c7bcb7fe5cc35
Jeppe Hasseriis <j.hasseriis@gmail.com>
2018-02-09 22:56:13 +0100

    Print number of queries and time in html comment

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
  *
  * Footer
  */
- 
+
 /**
  * Globalize $mdma_options
- * 
+ *
  * @global	array	$foxhound_options	Theme options array
  */
 global $foxhound_options;
@@ -19,28 +19,29 @@ global $foxhound_options;
 </div>
 
 	</div>
-	<?php 
+	<?php
 	global $post;
 	if ( 'template-full-width.php' != get_page_template_slug( $post->ID )  or  is_404() ) {
-		get_sidebar(); 
+		get_sidebar();
 	}
 	?>
 </div>
 
 <?php /* Footer */ ?>
 <div id="footer">
-		
+
 	<?php get_template_part( 'social-network-links' ); ?>
-					
+
 	<p>
 		<?php echo $foxhound_options['footer_text']; ?>
 		</br>
 		<em class="develop"><a href="http://foxhoundbandthemes.com/themes/dark-gritty-theme/">Dark N Gritty Theme</a> by <a href="http://foxhoundbandthemes.com">Foxhound Band Themes</a></em>
-		
+
 	</p>
-	
+
 </div>
 
 <?php wp_footer(); ?>
-</body> 
+<!-- <?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?> seconds. --> 
+</body>
 </html>
